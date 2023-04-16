32b440d67e20a7a81a7aa642635655c1df5d1dfa
sankarshandev98 <gsankar164@gmail.com>
2023-04-16 15:33:14 +0530

    update

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
-const connectDB = require("./config/dbConn");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
 
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(cookieParser());

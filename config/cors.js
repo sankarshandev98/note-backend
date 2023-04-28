1348604d9b412abb9c72b781b2e8b252f9885195
Sankarshan Goswami <84925828+sankarshandev98@users.noreply.github.com>
2023-04-28 11:38:29 -0700

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["https://mern-note.onrender.com", "http://localhost:3000"];
+const allowedOrigins = ["https://mern-note.onrender.com"];
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {

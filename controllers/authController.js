a0eb8b7d5617f01b4d42283ab07e0aa83d4e430b
sankarshandev98 <gsankar164@gmail.com>
2023-05-01 21:16:39 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,12 +40,12 @@ const login = async (req, res) => {
   );
 
   // Create secure cookie with refresh token
-  // res.cookie('jwt', refreshToken, {
-  //     httpOnly: true, //accessible only by web server
-  //     secure: true, //https
-  //     sameSite: 'None', //cross-site cookie
-  //     maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
-  // })
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, //accessible only by web server
+    secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+  });
 
   // Send accessToken containing username and roles
   res.json({ accessToken });

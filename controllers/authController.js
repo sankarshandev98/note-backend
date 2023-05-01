96902c5012cec6b96efdaeffb892a6431ccb4361
Sankarshan Goswami <84925828+sankarshandev98@users.noreply.github.com>
2023-05-01 07:20:07 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,9 +41,9 @@ const login = async (req, res) => {
 
     // Create secure cookie with refresh token 
     res.cookie('jwt', refreshToken, {
-//         httpOnly: true, //accessible only by web server 
-//         secure: true, //https
-//         sameSite: 'None', //cross-site cookie 
+        httpOnly: true, //accessible only by web server 
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie 
         maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
     })
 

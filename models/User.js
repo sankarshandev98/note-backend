a94105642b6b42111bc4917016a70b133d48367c
sankarshandev98 <gsankar164@gmail.com>
2023-05-03 22:14:07 +0530

    update

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     roles: {
         type: [String],
-        default: ["admin"]
+        default: ["Employee"]
     },
     active: {
         type: Boolean,
